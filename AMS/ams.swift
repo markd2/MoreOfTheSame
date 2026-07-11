98aa4825a10334355e77a2cf7a92c63f305bcf73
MarkD <markd@borkware.com>
2026-07-11 17:27:11 -0400

    Adds sanity check

--- AMS/ams.swift
+++ AMS/ams.swift
@@ -21,12 +21,55 @@ struct Cell {
 let grid = magicify(order: order)
 print(grid: grid, order: order)
 
+/*
+var count = 0
+
+while sanityCheck(grid: grid, order: order) {
+    count += 1
+    print("try \(count)")
+}
+*/
+
+// --------------------------------------------------
+func sanityCheck(grid: [[Cell]], order: Int) -> Bool {
+    // S = n(n^2 + 1) / 2
+    let expected = (order * (order*order + 1)) / 2
+    
+    for row in 0 ..< order {
+        var sum = 0
+        for c in 0 ..< order {
+            sum += grid[row][c].number
+        }
+        if sum != expected {
+            print("oops - row \(row) is \(sum), not \(expected)")
+            return false
+        }
+    }
+
+    for column in 0 ..< order {
+        var sum = 0
+        for r in 0 ..< order {
+            sum += grid[r][column].number
+        }
+        if sum != expected {
+            print("oops - column \(column) is \(sum), not \(expected)")
+            return false
+        }
+    }
+    return true
+}
+
 // --------------------------------------------------
 func magicify(order: Int) -> [[Cell]] {
     var grid = [[Cell]](repeating: [Cell](repeating: Cell(), count: order),
                         count: order)
+    /*
     var row = 0
     var column = order / 2
+     */
+
+    var row = Int.random(in: 0 ..< order)
+    var column = Int.random(in: 0 ..< order)
     var number = 1
     grid[row][column].number = number
 
@@ -35,8 +78,6 @@ func magicify(order: Int) -> [[Cell]] {
     while number <= order * order {
         if loopNumber > 50 { break }
         loopNumber += 1
-
-        print(grid: grid, order: order)
 
         let oldRow = row
         let oldColumn = column
@@ -49,25 +90,17 @@ func magicify(order: Int) -> [[Cell]] {
         if row < 0 { row = order - 1 }
         if column < 0 { column = order - 1}
 
-        print("looking at \(row) \(column)")
-
         if grid[row][column].number == 0 {
-            print("yay")
             number = number + 1
             grid[row][column].number = number
         } else {
-            print("collide")
-            // undo prior move
-
             // move down instead
             row = oldRow + 1
             column = oldColumn
 
             if row > order - 1 { row = 0 }
-            print("    colliding \(oldRow) \(oldColumn) -> \(row) \(column)")
 
             if grid[row][column].number != 0 {
-                print("this is occupied?")
                 continue
             }
             number = number + 1
@@ -83,7 +116,7 @@ func magicify(order: Int) -> [[Cell]] {
 func print(grid: [[Cell]], order: Int) {
     for row in 0 ..< order {
         for column in 0 ..< order {
-            let fmt = String(format: "%5d", grid[row][column].number)
+            let fmt = String(format: "%3d", grid[row][column].number)
             print(fmt, terminator: "")
         }
         print("\n")
