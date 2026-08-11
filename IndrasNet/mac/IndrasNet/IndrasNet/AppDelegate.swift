bb1f0da39d09ce29126a5f7c44391484aa96ce36
MarkD <markd@borkware.com>
2026-08-11 09:33:10 -0700

    adds some labels to make understanding what's going on with numbers

--- IndrasNet/mac/IndrasNet/IndrasNet/AppDelegate.swift
+++ IndrasNet/mac/IndrasNet/IndrasNet/AppDelegate.swift
@@ -13,11 +13,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet var window: NSWindow!
 
     @IBOutlet var indraView: IndraView!
+
     @IBOutlet var angularDivisionSlider: NSSlider!
+    @IBOutlet var angularDivisionLabel: NSTextField!
+
     @IBOutlet var centerOpeningSlider: NSSlider!
+    @IBOutlet var centerOpeningLabel: NSTextField!
+
     @IBOutlet var diamondSizeSlider: NSSlider!
+    @IBOutlet var diamondSizeLabel: NSTextField!
+
     @IBOutlet var diamondSpaceSlider: NSSlider!
+    @IBOutlet var diamondSpaceLabel: NSTextField!
+
     @IBOutlet var diamondGrowthSlider: NSSlider!
+    @IBOutlet var diamondGrowthLabel: NSTextField!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
@@ -26,6 +36,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         indraView.diamondSize = Unit(diamondSizeSlider.doubleValue)
         indraView.diamondSpace = Unit(diamondSpaceSlider.doubleValue)
         indraView.diamondGrowth = Unit(diamondGrowthSlider.doubleValue)
+
+        updateUI()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -38,24 +50,38 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 // UI Nonsense
 extension AppDelegate {
+    func updateUI() {
+        angularDivisionLabel.stringValue = "\(indraView.angularDivisions)"
+        
+        centerOpeningLabel.stringValue = "\(indraView.centerOpening)"
+        diamondSizeLabel.stringValue = "\(indraView.diamondSize)"
+        diamondSpaceLabel.stringValue = "\(indraView.diamondSpace)"
+        diamondGrowthLabel.stringValue = "\(indraView.diamondGrowth)"
+    }
+
     @IBAction func handleAngularDivisionSlider(_ sender: NSSlider) {
         indraView.angularDivisions = sender.integerValue
+        updateUI()
     }
 
     @IBAction func handleCenterOpeningSlider(_ sender: NSSlider) {
         indraView.centerOpening = Unit(sender.integerValue)
+        updateUI()
     }
 
     @IBAction func handleDiamondSizeSlider(_ sender: NSSlider) {
         indraView.diamondSize = Unit(sender.doubleValue)
+        updateUI()
     }
 
     @IBAction func handleDiamondSpaceSlider(_ sender: NSSlider) {
         indraView.diamondSpace = Unit(sender.doubleValue)
+        updateUI()
     }
 
     @IBAction func handleDiamondGrowthSlider(_ sender: NSSlider) {
         indraView.diamondGrowth = Unit(sender.doubleValue)
+        updateUI()
     }
 }
 
